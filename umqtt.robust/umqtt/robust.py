20f387e8d8dccd533dc6d3b2cef6035982e478ba
dcf1007 <dcf1007@gmail.com>
2020-03-24 18:24:31 +0100

    Update robust.py

--- umqtt.robust/umqtt/robust.py
+++ umqtt.robust/umqtt/robust.py
@@ -1,10 +1,10 @@
 import utime
-from . import simple
+#from . import simple
+import simple
 
 class MQTTClient(simple.MQTTClient):
-    ATTEMPTS=10
-    DELAY = 1
-    DEBUG = False
+    ATTEMPTS=0
+    DELAY = 0
 
     def __init__(self, client_id, server, port=0, user=None, password=None, keepalive=0,
                  ssl=False, ssl_params={}, attempts=10, delay=1):
@@ -12,38 +12,20 @@ class MQTTClient(simple.MQTTClient):
         self.ATTEMPTS = attempts
         self.DELAY = delay
 
-    def delay(self):
-        utime.sleep(self.DELAY)
-
-    def log(self, in_reconnect, e):
-        if self.DEBUG:
-            if in_reconnect:
-                print("mqtt reconnect: %r" % e)
-            else:
-                print("mqtt: %r" % e)
-
-    def reconnect(self):
+    def with_retry(self, meth, *args, **kwargs):
         i = 0
         while i < self.ATTEMPTS:
             try:
-                return self.connect(False)
+                self.connect(False)
+                return meth(*args, **kwargs)
             except OSError as e:
-                self.log(True, e)
+                print("%r" % e)
                 i += 1
-                self.delay()
+                utime.sleep(self.DELAY)
+        print("time out")
 
-    def publish(self, topic, msg, retain=False, qos=0):
-        while 1:
-            try:
-                return super().publish(topic, msg, retain, qos)
-            except OSError as e:
-                self.log(False, e)
-            self.reconnect()
+    def publish(self, *args, **kwargs):
+        return self.with_retry(super().publish, *args, **kwargs)
 
-    def wait_msg(self, blocking=True):
-        while 1:
-            try:
-                return super().wait_msg(blocking)
-            except OSError as e:
-                self.log(False, e)
-            self.reconnect()
+    def wait_msg(self, *args, **kwargs):
+        return self.with_retry(super().wait_msg, *args, **kwargs)
