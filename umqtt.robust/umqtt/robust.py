b5cf99b837bc6d387e3ee47073b11e8d4ff0fd8f
dcf1007 <dcf1007@gmail.com>
2020-03-24 16:21:53 +0100

    Update robust.py
    Improve coherence in umqtt.robust

--- umqtt.robust/umqtt/robust.py
+++ umqtt.robust/umqtt/robust.py
@@ -2,11 +2,17 @@ import utime
 from . import simple
 
 class MQTTClient(simple.MQTTClient):
-
-    DELAY = 2
+    ATTEMPTS=10
+    DELAY = 1
     DEBUG = False
 
-    def delay(self, i):
+    def __init__(self, client_id, server, port=0, user=None, password=None, keepalive=0,
+                 ssl=False, ssl_params={}, attempts=10, delay=1):
+        super().__init__(client_id, server, port, user, password, keepalive, ssl, ssl_params)
+        self.ATTEMPTS = attempts
+        self.DELAY = delay
+
+    def delay(self):
         utime.sleep(self.DELAY)
 
     def log(self, in_reconnect, e):
@@ -18,25 +24,13 @@ class MQTTClient(simple.MQTTClient):
 
     def reconnect(self):
         i = 0
-        while 1:
+        while i < self.ATTEMPTS:
             try:
                 return self.connect(False)
             except OSError as e:
                 self.log(True, e)
                 i += 1
-                self.delay(i)
-
-    def with_retry(self, meth, *args, **kwargs):
-        while 1:
-            try:
-                return meth(*args, **kwargs)
-            except OSError as e:
-                print("%r" % e)
-            time.sleep(0.5)
-            self.reconnect()
-
-    def publish_(self, *args, **kwargs):
-        return self.with_retry(super().publish, *args, **kwargs)
+                self.delay()
 
     def publish(self, topic, msg, retain=False, qos=0):
         while 1:
